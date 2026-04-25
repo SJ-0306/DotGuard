68330720ac3c9a7583a568143866a81a2e9d47d7
sj-0306 <swaminathanjag@gmail.com>
2026-04-25 13:37:59 -0400

    added scalability via tfvars for more easily adding extra config files

--- main.tf
+++ main.tf
@@ -1,7 +1,19 @@
+
+resource "local_file" "dotfiles" {
+  for_each = var.dotfiles
+  filename = each.value.destination_path
+  content = file("${path.module}/${each.value.source_path}")
+}
+
+
+
+
+/*
 resource "local_file" "kitty_config" {
   filename = var.kitty_config_path
   content  = file("${path.module}/confs/kitty.conf")
 }
+
 
 resource "local_file" "bashrc_config" {
   filename = var.bash_config_path
@@ -12,3 +24,4 @@ resource "local_file" "hyprland_config" {
   filename = var.hyprland_config_path
   content  = file("${path.module}/confs/hyprland.conf")
 }
+*/
--- variables.tf
+++ variables.tf
@@ -1,17 +1,6 @@
-variable "kitty_config_path" {
-  type        = string
-  default     = "/home/swami/.config/kitty/kitty.conf"
-  description = "config path to kitty term "
-}
-
-variable "bash_config_path" {
-  type        = string
-  default     = "/home/swami/.bashrc"
-  description = "config path to bashrc"
-}
-
-variable "hyprland_config_path" {
-  type        = string
-  default     = "/home/swami/.config/hypr/hyprland.conf"
-  description = "config path to hyprland config "
+variable "dotfiles" {
+  type = map(object({
+    source_path = string
+    destination_path = string
+  }))
 }
