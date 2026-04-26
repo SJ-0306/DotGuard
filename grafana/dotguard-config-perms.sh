a2a08f0744d6dedbac9cb5ee10a12ca16409e61d
sj-0306 <swaminathanjag@gmail.com>
2026-04-26 19:14:13 -0400

    waybar config, style, and color files added

--- grafana/dotguard-config-perms.sh
+++ grafana/dotguard-config-perms.sh
@@ -1,7 +1,8 @@
 
 KITTY_PERMS=$(stat -c "%a" /home/swami/.config/kitty/kitty.conf)
 HYPR_PERMS=$(stat -c "%a" /home/swami/.config/hypr/hyprland.conf)
-
+BASH_PERMS=$(stat -c "%a" /home/swami/.bashrc)
 
 echo "dotguard_config_permissions{file=\"kitty\"} $KITTY_PERMS" > /var/lib/node_exporter/textfile_collector/dotguard.prom
 echo "dotguard_config_permissions{file=\"hyprland\"} $HYPR_PERMS" >> /var/lib/node_exporter/textfile_collector/dotguard.prom
+echo "dotguard_config_permissions{file=\"bashrc\"} $BASH_PERMS" >> /var/lib/node_exporter/textfile_collector/dotguard.prom
