8e1dd0c28db1c1193588c1002b0d3b8f5f3c8667
sj-0306 <swaminathanjag@gmail.com>
2026-04-26 20:37:05 -0400

    added an ansible playbook automation for automatic drift prevention

--- grafana/dotguard-config-perms.sh
+++ grafana/dotguard-config-perms.sh
@@ -1,17 +1,24 @@
-#!/bin/bash
-OUTPUT_FILE="/var/lib/node_exporter/textfile_collector/dotguard.prom"
-> "$OUTPUT_FILE"
+ KITTY_PERMS=$(stat -c "%a" /home/swami/.config/kitty/kitty.conf)
 
-declare -A CONFIGS=(
-    ["kitty"]="$HOME/.config/kitty/kitty.conf"
-    ["hyprland"]="$HOME/.config/hypr/hyprland.conf"
-    ["bashrc"]="$HOME/.bashrc"
-    ["waybarconfig"]="$HOME/.config/waybar/config.jsonc"
-    ["waybarcolor"]="$HOME/.config/waybar/colors.css"
-    ["waybarstyle"]="$HOME/.config/waybar/style.css"
-)
+HYPR_PERMS=$(stat -c "%a" /home/swami/.config/hypr/hyprland.conf)
 
-for NAME in "${!CONFIGS[@]}"; do
-    PERMS=$(stat -c "%a" "${CONFIGS[$NAME]}" 2>/dev/null || echo "0")
-    echo "dotguard_config_permissions{file=\"$NAME\"} $PERMS" >> "$OUTPUT_FILE"
-done
+BASH_PERMS=$(stat -c "%a" /home/swami/.bashrc)
+
+WAYBARCONFIG_PERMS=$(stat -c "%a" /home/swami/.config/waybar/config.jsonc)
+
+WAYBARCOLOR_PERMS=$(stat -c "%a" /home/swami/.config/waybar/colors.css)
+
+WAYBARSTYLE_PERMS=$(stat -c "%a" /home/swami/.config/waybar/style.css)
+
+
+echo "dotguard_config_permissions{file=\"kitty\"} $KITTY_PERMS" > /var/lib/node_exporter/textfile_collector/dotguard.prom
+
+echo "dotguard_config_permissions{file=\"hyprland\"} $HYPR_PERMS" >> /var/lib/node_exporter/textfile_collector/dotguard.prom
+
+echo "dotguard_config_permissions{file=\"bashrc\"} $BASH_PERMS" >> /var/lib/node_exporter/textfile_collector/dotguard.prom
+
+echo "dotguard_config_permissions{file=\"waybarconfig\"} $WAYBARCONFIG_PERMS" >> /var/lib/node_exporter/textfile_collector/dotguard.prom
+
+echo "dotguard_config_permissions{file=\"waybarcolor\"} $WAYBARCOLOR_PERMS" >> /var/lib/node_exporter/textfile_collector/dotguard.prom
+
+echo "dotguard_config_permissions{file=\"waybarstyle\"} $WAYBARSTYLE_PERMS" >> /var/lib/node_exporter/textfile_collector/dotguard.prom
--- main.tf
+++ main.tf
@@ -3,5 +3,5 @@ resource "local_file" "dotfiles" {
   for_each        = var.dotfiles
   filename        = each.value.destination_path
   content         = file("${path.module}/${each.value.source_path}")
-  file_permission = each.value.mode
+  file_permission = format("%04s", each.value.mode)
 }
